fa4f1b6f2b2b31a1fff255da10f4988bf6246122
Shivanand <datasnekatpure@gmail.com>
2024-03-24 12:35:27 +0530

    Added data

---
+++ src/helper.py
@@ -0,0 +1,6 @@
+DEFAULT_SYSTEM_PROMPT="""\
+As an AI assistant, I am committed to providing helpful and accurate information about AI research papers. My goal is to simplify complex technical content and explain it in easy-to-understand language. 
+If you have any questions related to AI research, feel free to ask, and I'll do my best to assist you!."""
+
+CUSTOM_SYSTEM_PROMPT="You are an advanced assistant that provides answers related to research in Artifitial Intelligence and Machine Learning"
+
---
+++ src/run_local.py
@@ -0,0 +1,23 @@
+from langchain import PromptTemplate
+from langchain import LLMChain
+from langchain.llms import CTransformers
+from src.helper import *
+
+B_INST, E_INST = "[INST]", "[/INST]"
+B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+
+instruction = "Please provide a concise answer in simple English under 500 words. Avoid repetition: \n\n {question}"
+
+SYSTEM_PROMPT = B_SYS + CUSTOM_SYSTEM_PROMPT + E_SYS
+template = B_INST + SYSTEM_PROMPT + instruction + E_INST
+
+prompt = PromptTemplate(template=template, input_variables=['question'])
+
+llm = CTransformers(model='model/llama-2-7b-chat.ggmlv3.q4_0.bin',
+                    model_type='llama',
+                    config={'max_new_tokens': 512,
+                            'temperature': 0.01}
+                   )
+LLM_Chain=LLMChain(prompt=prompt, llm=llm)
+
+print(LLM_Chain.run("Explain transformers architecture in brief"))
