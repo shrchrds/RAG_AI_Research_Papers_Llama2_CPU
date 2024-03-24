2cbc80263739dd0b3484c5bc7511a36d041d62bb
Shivanand <datasnekatpure@gmail.com>
2024-03-24 11:23:18 +0530

    Added Requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'RAG AI Research Papers on CPU',
+    version= '0.0.0',
+    author= 'Shivanand',
+    author_email= 'datasnekatpure@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
